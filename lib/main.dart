e8d908af19ff9437a82e0ce74d070f60b5c3f92f
Bherliane Mallari <bherlianemhae.mallari01@gmail.com>
2026-07-24 09:23:27 +0800

    feat: add app state

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const StateManagementActivity());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeModel(),
+      child: const MyApp(),
+    )
+  );
 }
 
-class StateManagementActivity extends StatelessWidget {
-  const StateManagementActivity({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final themeModel = Provider.of<ThemeModel>(context);
 
-    return const MaterialApp(
-      home: const MyHomePage(),
+    return MaterialApp(
+      theme: themeModel.isDark ? ThemeData.dark() : ThemeData.light(),
+      home: const MyHome(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class ThemeModel extends ChangeNotifier {
+  bool _isDark = false;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  bool get isDark => _isDark;
+
+  void toggleTheme() {
+    _isDark = !_isDark;
+    notifyListeners();
+  }
 }
 
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class MyHome extends StatelessWidget {
+  const MyHome({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final themeModel = Provider.of<ThemeModel>(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Ephemeral State Example'),
+        title: const Text('App State Example'),
+        actions: [
+          Switch(
+            value: themeModel.isDark,
+            onChanged: (_) => themeModel.toggleTheme()
+          ),
+        ],
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Text('Toggle the theme using the switch in the app bar.'),
       ),
     );
   }
